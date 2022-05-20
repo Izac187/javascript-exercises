208d1144bdc22a97f3232db2ec0c9f28a35027c8
Izac187 <isalinas187@gmail.com>
2022-05-19 19:02:56 -0700

    Function completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let newString = word.split('');
+    newString.reverse();
+    let stringReversed = newString.join('');
+    return stringReversed;
 };
 
 // Do not edit below this line
